2766b5fdda200c6327398f59e3ee223161d9730a
Ridho Dimas <ridhoyudant22@gmail.com>
2025-05-20 15:03:46 +0700

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,12 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'splash',
+    loadComponent: () => import('./pages/splash/splash.page').then( m => m.SplashPage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
 ];
